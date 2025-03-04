4a2294169493bf1b3effafdabb20a23902ad3053
ritmo0220 <uraberitmo@gmail.com>
2025-03-04 14:05:27 +0900

    ライントレースのコード

--- main01.rb
+++ main01.rb
@@ -11,19 +11,33 @@
    
    
    
-   while 
+   while true
            if (lux_right.read_raw <= 100)
-           # 左右モーター停止
-           lm_pin1.duty(100)
+           # 左右モーター逆転
+           lm_pin1.duty(0)
+           lm_pin2.duty(100)
+           rm_pin1.duty(0)
+           rm_pin2.duty(100)
+           sleep 0.3
+           # 回転
+           lm_pin1.duty(0)
            lm_pin2.duty(100)
            rm_pin1.duty(100)
-           rm_pin2.duty(100)
+           rm_pin2.duty(0)
+           sleep 0.3
            elsif (lux_left.read_raw <= 100)
-               # 左右モーター停止
-               lm_pin1.duty(100)
-               lm_pin2.duty(100)
-               rm_pin1.duty(100)
-               rm_pin2.duty(100)
+           # 左右モーター逆転
+           lm_pin1.duty(0)
+           lm_pin2.duty(100)
+           rm_pin1.duty(0)
+           rm_pin2.duty(100)
+           sleep 0.3
+           # 回転
+           lm_pin1.duty(100)
+           lm_pin2.duty(0)
+           rm_pin1.duty(0)
+           rm_pin2.duty(100)
+           sleep 0.3
        end
        
        # 左右モーター出力30%正回転
@@ -32,7 +46,4 @@
        rm_pin1.duty(30)
        rm_pin2.duty(0)
        sleep (0.1)
-   
-
-       
    end
