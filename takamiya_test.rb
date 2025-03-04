690667eec791f2b4005c5df316052b2682ed391c
Yuriko <yuriko.lily@icloud.com>
2025-03-04 16:33:51 +0900

    test

--- takamiya_test.rb
+++ takamiya_test.rb
@@ -21,13 +21,13 @@ lm_pin2 = PWM.new(33) # 左モーターPIN2
 # 左右モーター出力100%正回転
 lm_pin1.duty(45)
 lm_pin2.duty(0)
-rm_pin1.duty(41)
+rm_pin1.duty(42)
 rm_pin2.duty(0)
 
 sleep 3.2
 
 # Eで回転
-lm_pin1.duty(80)
+lm_pin1.duty(90)
 lm_pin2.duty(0)
 rm_pin1.duty(0)
 rm_pin2.duty(0)
@@ -35,7 +35,7 @@ rm_pin2.duty(0)
 sleep 1
 
 # Dに向かう
-lm_pin1.duty(30)
+lm_pin1.duty(50)
 lm_pin2.duty(0)
 rm_pin1.duty(50)
 rm_pin2.duty(0)
@@ -43,12 +43,12 @@ rm_pin2.duty(0)
 sleep 1.5
 
 # Bに向かう
-lm_pin1.duty(30)
+lm_pin1.duty(40)
 lm_pin2.duty(0)
 rm_pin1.duty(50)
 rm_pin2.duty(0)
 
-sleep 1.5
+sleep 2
 
 #Aに向かう
 lm_pin1.duty(30)
@@ -56,7 +56,7 @@ lm_pin2.duty(0)
 rm_pin1.duty(50)
 rm_pin2.duty(0)
 
-sleep 1.5
+sleep 2
 
 #Cに向かう
 lm_pin1.duty(50)
