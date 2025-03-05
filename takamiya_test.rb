42b147d9f2e17df7f75f98ae3b99b0765988a69a
Yuriko <yuriko.lily@icloud.com>
2025-03-05 10:28:33 +0900

    共同作業用

--- takamiya_test.rb
+++ takamiya_test.rb
@@ -1,82 +1,47 @@
-# モーターの回転制御
-# モーター毎のPIN1・PIN2にそれぞれ電圧を掛け、その差によって回転が変化する。
-# 電圧の強さは、周波数とduty比（パルスの立ち上がり比率）の数値によってコントロール可能。
-# 
-# 蟹ロボのモーターは以下の仕様で制御できる。
-# 
-# PIN1: high, PIN2: low  #=> 正回転
-# PIN1: low,  PIN2: high #=> 逆回転
-# PIN1: high, PIN2: high #=> ブレーキ
-# PIN1: low,  PIN2: low  #=> low-power automatic sleep mode
+require 'gosu'
 
-# 右モーター初期化（引数のGPIO番号は全ての蟹ロボで共通）
-rm_pin1 = PWM.new(25) # 右モーターPIN1
-rm_pin2 = PWM.new(26) # 右モーターPIN2
+class Player
+  attr_reader :x, :y
 
-# 左モーター初期化（引数のGPIO番号は全ての蟹ロボで共通）
-lm_pin1 = PWM.new(32) # 左モーターPIN1
-lm_pin2 = PWM.new(33) # 左モーターPIN2
+  def initialize
+    @x = 720  # 初期X座標（右上からスタート）
+    @y = 90  # 初期Y座標
+    @speed = 1 # 移動速度
+    @image = Gosu::Image.new("images/kani.png", tileable: false)
+  end
 
+  def update
+    @x -= @speed  # X座標を減らして左へ移動
+    @y += @speed  # Y座標を増やして下へ移動
 
-# 左右モーター出力100%正回転
-lm_pin1.duty(45)
-lm_pin2.duty(0)
-rm_pin1.duty(42)
-rm_pin2.duty(0)
+    # 画面外に出たら反対側にワープ（640x480の画面サイズを想定）
+    @x = 640 if @x < 0
+    @y = 0 if @y > 480
+  end
 
-sleep 3.2
+  def draw
+    @image.draw(@x, @y, 0)
+  end
+end
 
-# Eで回転
-lm_pin1.duty(90)
-lm_pin2.duty(0)
-rm_pin1.duty(0)
-rm_pin2.duty(0)
+class MyWindow < Gosu::Window
+  def initialize
+    super 800, 600
+    self.caption = 'RubyCamp2025SP tutorial'
+    @player = Player.new
+    @start_time = Gosu.milliseconds  # ゲーム開始時刻を記録
+    @e_time = 2000 # 2秒後にゲーム終了
+  end
 
-sleep 1
+  def update
+    if Gosu.milliseconds - @start_time < @e_time
+    @player.update
 
-# Dに向かう
-lm_pin1.duty(50)
-lm_pin2.duty(0)
-rm_pin1.duty(50)
-rm_pin2.duty(0)
+  end
 
-sleep 1.5
+  def draw
+    @player.draw
+  end
+end
 
-# Bに向かう
-lm_pin1.duty(40)
-lm_pin2.duty(0)
-rm_pin1.duty(50)
-rm_pin2.duty(0)
-
-sleep 2
-
-#Aに向かう
-lm_pin1.duty(30)
-lm_pin2.duty(0)
-rm_pin1.duty(50)
-rm_pin2.duty(0)
-
-sleep 2
-
-#Cに向かう
-lm_pin1.duty(50)
-lm_pin2.duty(0)
-rm_pin1.duty(40)
-rm_pin2.duty(0)
-
-sleep 2
-
-#ゴール
-lm_pin1.duty(50)
-lm_pin2.duty(0)
-rm_pin1.duty(20)
-rm_pin2.duty(0)
-
-sleep 1
-
-# 左右モーター停止
-lm_pin1.duty(100)
-lm_pin2.duty(100)
-rm_pin1.duty(100)
-rm_pin2.duty(100)
-
+MyWindow.new.show
