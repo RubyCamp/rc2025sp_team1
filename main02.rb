d8ed2f3d38ee15ed007ecc2d260beab59fd7d082
ritmo0220 <uraberitmo@gmail.com>
2025-03-05 15:08:37 +0900

    更新

--- main02.rb
+++ main02.rb
@@ -11,15 +11,10 @@ lux_left  = ADC.new(2)  # 左ライトセンサー初期化（GPIO番号: 2）
 
 def kanimove(t) 
     @lm_pin1.duty(100)
-    @lm_pin2.duty(80)
-    @rm_pin1.duty(100)
-    @rm_pin2.duty(60)
-    sleep 0.5
-    @lm_pin1.duty(100)
     @lm_pin2.duty(69)
     @rm_pin1.duty(100)
-    @rm_pin2.duty(72)
-    sleep t - 0.5
+    @rm_pin2.duty(71)
+    sleep t
     puts "#{t} 秒間移動しました"
 end
 
@@ -27,21 +22,18 @@ end
 @r = 0
 @l = 1
 def kanirotate(t, rl) # (秒数*0.2,右=0 左=1)
-        @rm_pin1.duty(15)
-        @rm_pin2.duty(40)
-        sleep 0.1
     if rl == 0 
-        @lm_pin1.duty(40)
-        @lm_pin2.duty(15)
-        @rm_pin1.duty(15)
-        @rm_pin2.duty(40)
+        @lm_pin1.duty(100)
+        @lm_pin2.duty(75)
+        @rm_pin1.duty(75)
+        @rm_pin2.duty(100)
         sleep t
         puts "#{t} 秒間右回転しました"
 elsif
-        @lm_pin1.duty(15)
-        @lm_pin2.duty(40)
-        @rm_pin1.duty(40)
-        @rm_pin2.duty(15)
+        @lm_pin1.duty(75)
+        @lm_pin2.duty(100)
+        @rm_pin1.duty(100)
+        @rm_pin2.duty(75)
         sleep t
         puts "#{t} 秒間左回転しました"
     end
@@ -55,35 +47,43 @@ def brake
     puts "ブレーキを掛けました"
 end
 
+    # @rm_pin1.duty(100)
+    # @rm_pin2.duty(80)
+    # sleep 0.2
+        
+        brake()
 #回転してEの方向を向く
 kanirotate(0.5,@r)
 
 #Eまで動く
-kanimove(10)
+kanimove(9)
 
-#E,Dまで移動
-kanimove(6)
+#Dの方向を向く
+kanirotate(4,@r)
+
+#Dまで移動
+kanimove(8)
 
 #回転、Bの方向を向く
-kanirotate(1,@l)
+kanirotate(0.7,@l)
 
 #Bまで移動
-kanimove(4)
+kanimove(8)
 
-#回転、Aの方向を向く
-kanirotate(3,@l)
+# #回転、Aの方向を向く
+# kanirotate(3,@l)
 
-#Aまで移動
-kanimove(7)
+# #Aまで移動
+# kanimove(7)
 
-#回転、Cの方向を向く
-kanirotate(8,@l)
+# #回転、Cの方向を向く
+# kanirotate(8,@l)
 
-#Cまで移動
-kanimove(15)
+# #Cまで移動
+# kanimove(15)
 
-#回転、ゴールの方向を向く
-kanirotate(6,@r)
+# #回転、ゴールの方向を向く
+# kanirotate(6,@r)
 
 #カニをゴールへシュート！超！エキサイティング！
 brake()
